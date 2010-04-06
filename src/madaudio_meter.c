cfecaa6904215fbf0a9c3c13096955b177358bae
Alexander V. Nikolaev <avn@daemon.hole.ru>
2010-04-07 01:27:30 +0300

    * Implement new progressbar * Drop edrawable dependency * Complete horizontal face

--- src/madaudio_meter.c
+++ src/madaudio_meter.c
@@ -2,25 +2,17 @@
 #include <stdio.h>
 #include <Evas.h>
 #include <Edje.h>
-#include <edrawable.h>
+#include <libeoi_themes.h>
+#include <libeoi_utils.h>
 
-#define METER_WIDTH   275
-#define METER_HEIGHT  10
 
 static void
 madaudio_create_meter(Evas_Object *gui, const char *name, const char *swallow)
 {
-    int w, h;
-    assert(gui);
-    edje_object_part_geometry_get(gui, swallow, NULL, NULL, &w, &h);
     Evas *evas = evas_object_evas_get(gui);
-    assert(evas);
-    printf("%p %p %d %d\n", evas, gui, w, h);
-    Evas_Object *meter = edrawable_add(evas, w, h);
+    Evas_Object *meter = eoi_create_themed_edje(evas, "madaudio", "meter");
     if(!meter)
         printf("epic fail: can't create meter\n");
-    evas_object_move(meter, 0, 0);
-    evas_object_resize(meter, w, h);
     evas_object_name_set(meter, name);
     evas_object_show(meter);
     edje_object_part_swallow(gui, swallow, meter);
@@ -47,15 +39,8 @@ madaudio_update_meter(Evas_Object* gui, const char *name,
         printf("epic fail: Can't find meter\n");
         return;
     }
-    edrawable_set_colors(meter, 0xd1, 0xd1, 0xd1, 255);
-    edrawable_draw_rectangle_fill(meter, 0, 0, w, h);
-    edrawable_set_colors(meter, 0, 0, 0, 255);
-    edrawable_draw_rectangle_fill(meter, 0, h-3,
-                            w, h);
-    if(value)
-    {
-        int width = w * value / 100 ;
-        edrawable_draw_rectangle_fill(meter, 0, 0, width, h);
-    }
-    edrawable_commit(meter);
+    char *src = xasprintf("%02f", 0.01 * (float) value );
+    edje_object_signal_emit(meter, "set-meter", src);
+//    printf("meter: %s\n", src);
+    free(src);
 }
